ce6c3974d50bf8d32b28c7f97d6d9df5d2731d0b
thechandi <chandiprakash16@gmail.com>
2019-05-11 00:15:05 +0545

    can access songs from the device

--- src/components/screens/MusicScreen.js
+++ src/components/screens/MusicScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, PermissionsAndroid } from "react-native";
+import { Text, View, FlatList } from "react-native";
+import MusicFiles from "react-native-get-music-files";
 
 export default class MusicScreen extends Component {
   state = {
@@ -7,39 +8,63 @@ export default class MusicScreen extends Component {
   };
 
   componentDidMount = () => {
-    // PermissionsAndroid.request("android.permission.READ_EXTERNAL_STORAGE").then(
-    //   res => {
-    //     console.log(res);
-    //     this.setState({ musicData: res });
-    //   }
-    // );
-
-    // // .then(err => console.log(err));
-    this.requestExternalStoreageRead();
+    MusicFiles.getAll({
+      blured: true, // works only when 'cover' is set to true
+      artist: true,
+      duration: true, //default : true
+      cover: true, //default : true,
+      genre: true,
+      title: true,
+      cover: true,
+      minimumSongDuration: 10000 // get songs bigger than 10000 miliseconds duration,
+    })
+      .then(tracks => {
+        console.log(tracks);
+        this.setState({
+          musicData: tracks
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
-  async requestExternalStoreageRead() {
-    alert("sd");
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-        {
-          title: "React-Native Music player",
-          message: "App needs access to external storage"
-        }
-      );
-
-      return granted == PermissionsAndroid.RESULTS.GRANTED;
-    } catch (err) {
-      //Handle this error
-      return false;
-    }
-  }
+  showSongs = item => {
+    const { fileName } = item;
+    return (
+      <View style={{ marginTop: "5%", marginHorizontal: "3%" }}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 15,
+            fontFamily: "OpenSans-Regular"
+          }}
+        >
+          {fileName}
+        </Text>
+      </View>
+    );
+  };
 
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "#333645" }}>
-        <View />
+        <View style={{ height: 50, backgroundColor: "#C75E65" }}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              fontFamily: "OpenSans-Bold",
+              color: "#F7E1B8"
+            }}
+          >
+            Songs
+          </Text>
+        </View>
+        <FlatList
+          data={this.state.musicData}
+          renderItem={({ item }) => this.showSongs(item)}
+        />
       </View>
     );
   }
